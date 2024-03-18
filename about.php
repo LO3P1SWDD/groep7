c0f0d087a50479ad74af0bcc02e2f278ff814c09
gabriel da silva braga <6036046@mborijnland.nl>
2024-03-18 11:59:24 +0100

    menu
    menu een beetje aangepast

--- about.php
+++ about.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="The about us page from the website piazzo'spizza">
+        <meta name="author" content="Gabrielsoldaatvanagga">
+        <link rel="stylesheet" href="css/Restaurant.css">
+    <title>About us</title>
 </head>
 <body>
-    
+    <?php include('../view/header.php'); ?>
 </body>
 </html>
---
+++ menu.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="De menu van de website piazzo'spizza">
+    <meta name="author" content="Gabriel">
+    <link rel="stylesheet" href="Restaurant.css">
+    <title>Our Menu</title>
+</head>
+<body>
+<header>
+        <nav>
+          <div class="logo">
+            <a href="Homepage.html"><img src="img/26f9c961-a978-40b2-bc45-63442c0d7da3.png" alt="Home"></a>
+          </div>
+          <ul>
+            <li><a href="#"> Our Menu</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">About us</a></li>
+          </ul>
+        </nav>
+      </header>
+    <main></main>
+    <footer>
+        <a href="https://discord.com/channels/1216447600056598598/1216447600803188887"><div class="foto-container">
+          <img src="img/png-clipart-email-business-hotel-florida-gulf-coast-university-organization-discord-logo-angle-service-removebg-preview.png" alt="foto 1"></a>
+         <a href="https://www.instagram.com/gjabiagga/"> <img src="img/8608cce40e22afd979e926507479a909-removebg-preview.png" alt="foto 2"></a>
+         <a href="https://twitter.com/geertwilderspvv?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"> <img src="img/87de5128049f5fa9ac124b80434b08da-removebg-preview.png" alt="foto 3"></a>
+        </div>
+      </footer>
+    
+</body>
+</html>
